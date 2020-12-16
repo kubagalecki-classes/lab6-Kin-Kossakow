055466f9baad5801dec18adb0989f0f2d11c5438
KinKossakow <kinga.kossakowska.stud@pw.edu.pl>
2020-12-16 20:10:01 +0000

    rozwiazane

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,7 +10,18 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(),[](Human& h){h.birthday();} );
+
+  auto check = [](Human& tmp) {
+        if (tmp.isMonster()) {
+            return 'n';
+        }
+        else {
+            return 'y';
+        }
+    };
+
+    std::transform(people.begin(),people.end(), ret_v.rbegin(),check);
 
     return ret_v;
 }
